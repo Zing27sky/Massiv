66ec46191c44a4eb66a6fa751451fbdaa508211a
Zing27sky <Zing27@mial.ru>
2023-12-16 13:03:18 +0300

    как убрать устые элементы?.

--- src/Main.java
+++ src/Main.java
@@ -5,3 +5,17 @@ public class Main {
         searchForDuplicates();
     }
 
+    private static void searchForDuplicates() {
+        String[] arr = {"1", "2", "3", "2", "3", "4", "5"};
+        for (int i = 0; i < arr.length; i++) {
+            for (int i1 = i + 1; i1 < arr.length; i1++) {
+                if (arr[i].equals(arr[i1])) {
+                    arr[i1] = ("");
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
