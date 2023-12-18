96a2406565f8b102b7b13ab729fc56e94a17d90e
Zing27sky <Zing27@mial.ru>
2023-12-18 08:39:29 +0300

    afd

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         searchForDuplicates();
     }
 
     private static void searchForDuplicates() {
+        String result = "";
         String[] arr = {"1", "2", "3", "2", "3", "4", "5"};
-        for (int i = 0; i < arr.length; i++) {
-            for (int i1 = i + 1; i1 < arr.length; i1++) {
-                if (arr[i].equals(arr[i1])) {
-                    arr[i1] = ("");
-                }
-            }
-        }
 
         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i].equals(arr[j])) {
+                        arr[j] = null;
+                    }
+                }
+                result += arr[i];
+            }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(result);
     }
 }
